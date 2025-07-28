0c2044b7fb95fef5f04ff503fb2e9ba27f14a238
arndelp <arndelp@yahoo.fr>
2025-07-28 11:16:44 +0200

    debug lien

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import "./Carousel.css";
 import { Outlet, Link } from "react-router-dom";
+
 
 export const Carousel = ( { data }) => {
     
@@ -26,15 +27,15 @@ export const Carousel = ( { data }) => {
             </span>
             <p className="slogan d-none d-lg-block">CHOCO PAP</p>
             <p className="sloganM d-block d-lg-none">CHOCO PAP</p>
-        <div class="buttonPosition d-none d-lg-block">
+        <div className="buttonPosition d-none d-lg-block">
             <Link to="/boutique">
-                <button type="button" class=" btn-secondary btn-lg indexShopButton">VOIR LA BOUTIQUE</button>
+                <button type="button" className=" btn-secondary btn-lg indexShopButton">VOIR LA BOUTIQUE</button>
             </Link> 
         </div>
    
-        <div class="buttonPositionM d-block d-lg-none">
+        <div className="buttonPositionM d-block d-lg-none">
             <Link to="/boutique">
-                <button type="button" class=" btn-secondary btn-lg indexShopButtonM">VOIR LA BOUTIQUE</button>
+                <button type="button" className=" btn-secondary btn-lg indexShopButtonM">VOIR LA BOUTIQUE</button>
             </Link> 
             <Outlet />
         </div>
