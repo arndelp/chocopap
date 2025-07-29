7ff7082d9d7a1441f48e1309ba06aec152fd01c6
arndelp <arndelp@yahoo.fr>
2025-07-29 19:41:17 +0200

    build

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -34,7 +34,9 @@ function Nav() {
                     <ul className="navbar__links ">
                     
                         <li className="navbar__item">
-                            <a href='/chocopap/' className="navbar__link nbrArticle">Accueil</a>
+                            <Link to="/">
+                            <p href='/chocopap/' className="navbar__link nbrArticle">Accueil</p>
+                            </Link>
                         </li>
                         <li className="navbar__item">
                             <Link to="/boutique">
@@ -42,7 +44,9 @@ function Nav() {
                             </Link>
                         </li>   
                         <li className="navbar__item">
-                            <a href='/chocopap/' className="navbar__link nbrArticle" >&nbsp;&nbsp;2<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="cartIcon"><path fill="#ffd543" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg></a>
+                            <Link to="/">
+                                <p href='/chocopap/' className="navbar__link nbrArticle" >&nbsp;&nbsp;2<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="cartIcon"><path fill="#ffd543" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg></p>
+                            </Link>
                         </li>                  
                     </ul>
                 </div>
@@ -54,7 +58,9 @@ function Nav() {
                     
                     <ul className="navbar__links d-block d-lg-none">                   
                         <li className="navbar__item">
-                            <a href='/chocopap/' className="navbar__link">Accueil</a>
+                            <Link to="/">
+                                <p href='/chocopap/' className="navbar__link">Accueil</p>
+                            </Link>
                         </li>
                         <li className="navbar__item">
                             <Link to="/boutique">
@@ -62,7 +68,9 @@ function Nav() {
                             </Link>
                         </li>   
                         <li className="navbar__item">
-                            <a href='/chocopap/' className="navbar__link">Panier</a><a className="nbrArticle" >&nbsp;&nbsp;2</a><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="cartIcon"><path fill="#ffd543" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
+                            <Link to="/">
+                                <p href='/' className="navbar__link">Panier</p><a className="nbrArticle" >&nbsp;&nbsp;2</a><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="cartIcon"><path fill="#ffd543" d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
+                            </Link>
                         </li>                  
                     </ul>
 
